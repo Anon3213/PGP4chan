8e4a5ab6d26c9bae758342928ef12bba8e32d63a
Anon3213 <4chanthings@pm.me>
2023-04-23 16:07:46 -0500

    Implemented manual decryption button and basic error handling if decryption fails

--- src/index.ts
+++ src/index.ts
@@ -14,24 +14,42 @@ import * as openpgp from './openpgp.min.js'; // Remove from final script
 (async () => {
     'use strict';
 
+    // Button for manually decrypting posts in a thread
+    const decryptButton = document.createElement('button');
+    decryptButton.innerText = 'Decrypt';
+    decryptButton.addEventListener('click', DecryptAllPosts);
+    document.body.appendChild(decryptButton);
+
     // Keys have to be strictly formatted including '\n's
     const privateKeyArmored = `...private key goes here...`;
     const { readPrivateKey } = openpgp;
     const privateKey = await readPrivateKey({ armoredKey: privateKeyArmored });
     const pgpRegex = /-----BEGIN PGP MESSAGE-----[\s\S]+-----END PGP MESSAGE-----/;
     const replies = document.getElementsByClassName('postMessage');
-    for (let i = 0; i < replies.length; i++) {
-        const reply: string = replies[i].textContent ? replies[i].textContent : '';
-        if (pgpRegex.test(reply)) {
-            const pgpMessage = pgpRegex.exec(reply)?.toString();
-            // Reformatting the raw text from a post back into the correct PGP message format including '\n's
-            const formattedMessage = pgpMessage.slice(0, 27) + '\n\n' + pgpMessage.slice(27,-25) + '\n'+ pgpMessage.slice(-25);
-            const { readMessage, decrypt } = openpgp;
-            const decrypted = await decrypt({
-                message: await readMessage({ armoredMessage: formattedMessage }),
-                decryptionKeys: privateKey
-            });
-            replies[i].textContent = reply.replace(pgpRegex, `${decrypted.data}`);
+    
+    async function DecryptAllPosts() {
+        for (let i = 0; i < replies.length; i++) {
+            const reply = replies[i].textContent;
+            if (pgpRegex.test(reply)) {
+                const pgpMessage = pgpRegex.exec(reply)?.toString();
+                // Reformatting the raw text from a post back into the correct PGP message format including '\n's
+                const formattedMessage = pgpMessage.slice(0, 27) + '\n\n' + pgpMessage.slice(27,-25) + '\n'+ pgpMessage.slice(-25);
+                const { readMessage, decrypt } = openpgp;
+                let decrypted: { data: string; };
+                try {
+                        decrypted = await decrypt({
+                        message: await readMessage({ armoredMessage: formattedMessage }),
+                        decryptionKeys: privateKey
+                    });
+                } catch (err) {
+                    console.log(`Unable to decrypt message: ${pgpMessage}`);
+                }
+                if (decrypted === undefined) {
+                    replies[i].textContent = reply.replace(pgpRegex, 'ERROR: Unable to decrypt');
+                } else {
+                    replies[i].textContent = reply.replace(pgpRegex, `${decrypted.data}`);
+                }
+            }
         }
     }
 })();
